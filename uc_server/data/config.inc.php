3484dc2ff669e31c4254720414d1da8e2c164e51
songsuccess <songsuccess>
2019-02-25 14:33:33 +0800

    fix error

---
+++ uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', '192.168.5.88');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '1qaz2wsx');
+define('UC_DBNAME', 'moresec');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'a62b7b33df99906d9e9789d1ce867d38');
+define('UC_FOUNDERSALT', '<@!*hash_password*@!>');
+define('UC_KEY', 'LdM0g1JX2CxUDBlOjEXddciSwL6YseXHOAC6121SKRgjgbDJrnnidEwH4qkwlipa');
+define('UC_SITEID', 'L8j5814dZe8blf50ifPct3w6d081DeCdS4a7n1XaK5Cbn7J7IfH988H2m8n8Ocr1');
+define('UC_MYKEY', 'p8B5B18dneCblfQ0bfUcJ3x6R051ieldT4g7u1aa95zbP7F7mfR9d8b2U8V8Jcu1');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
